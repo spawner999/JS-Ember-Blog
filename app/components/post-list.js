cc44c1910f182a28f55e47d76be3b3d95289ca63
spawner999 <lore.ferrario@gmail.com>
2016-03-30 19:58:09 -0800

    add some styling and basic sorting list functionality

--- app/components/post-list.js
+++ app/components/post-list.js
@@ -1,12 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortedlist: Ember.computed.sort('postList','sorting'),
+  sorting: ['date:desc'],
   actions: {
     sendUpdate(currentPost, params){
       this.sendAction('sendUpdate', currentPost, params);
     },
     sendDelete(currentPost){
       this.sendAction('sendDelete', currentPost);
+      console.log(this.get('sortedlist'));
     }
   }
 });
